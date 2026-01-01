55ae64dfd9d9e278150bb42095d8af8a7340b73c
Lenny Minai <wlennyminai@gmail.com>
2026-01-02 00:10:58 +0300

    trial 2

---
+++ oneliners.py
@@ -0,0 +1,14 @@
+pip install numpy
+
+import numpy as np
+import pandas as pd
+from collections import Counter
+import statistics
+
+#sample datasets
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+grades = [ 78, 85, 90, 95, 100, 88, 76, 84, 92, 89 ]
+sales_data = [ 1200, 1500, 1300, 1700, 1600, 1800, 2000, 1900, 2100, 2200 ]
+temperatures = [ 30, 32, 31, 29, 28, 35, 33, 34, 36, 37 ]
+
+print(numbers) 
